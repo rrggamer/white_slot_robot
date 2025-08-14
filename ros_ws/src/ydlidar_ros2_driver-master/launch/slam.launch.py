5706b61d8c0041a41110bd85014cee76fa115bcf
papa <phakaewpra@gmail.com>
2025-08-14 13:43:07 +0000

    slam.rviz

--- ros_ws/src/ydlidar_ros2_driver-master/launch/slam.launch.py
+++ ros_ws/src/ydlidar_ros2_driver-master/launch/slam.launch.py
@@ -1,53 +1,43 @@
-#!/usr/bin/python3
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    # YDLidar parameters
-    share_dir = get_package_share_directory('ydlidar_ros2_driver')
-    param_file = LaunchConfiguration('params_file')
-    
-    declare_params = DeclareLaunchArgument(
-        'params_file',
-        default_value=os.path.join(share_dir, 'params', 'ydlidar_x3.yaml'),
-        description='Path to YDLidar parameters file'
-    )
-
-    # LiDAR node
+    # YDLidar driver
     lidar_node = Node(
         package='ydlidar_ros2_driver',
         executable='ydlidar_ros2_driver_node',
         name='ydlidar_ros2_driver_node',
         output='screen',
-        emulate_tty=True,
-        parameters=[param_file]
+        parameters=[os.path.join(get_package_share_directory('ydlidar_ros2_driver'),
+                                 'params', 'ydlidar_x3.yaml')]
     )
 
-    # Static TF from base_link -> laser
+    # Static TF from base_link to laser
     tf_node = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
         name='static_tf_pub_laser',
-        arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','laser_frame']
+        arguments=['0','0','0.02','0','0','0','1','base_link','laser_frame']
     )
 
-    # SLAM Toolbox online node
+    # SLAM Toolbox in online async mode
     slam_node = Node(
         package='slam_toolbox',
-        executable='async_slam_toolbox_node',  # Online SLAM
+        executable='async_slam_toolbox_node',
         name='slam_toolbox',
         output='screen',
-        parameters=[{'use_sim_time': False}],
-        remappings=[('/scan','/scan')]
+        parameters=[{
+            'use_sim_time': False,
+            'slam_mode': 'mapping',
+            'scan_topic': '/scan',
+            'map_frame': 'map',
+            'base_frame': 'base_link',
+            'odom_frame': 'odom',
+            'scan_topic': '/scan',
+            'publish_map': True
+        }]
     )
 
-    return LaunchDescription([
-        declare_params,
-        lidar_node,
-        tf_node,
-        slam_node
-    ])
+    return LaunchDescription([lidar_node, tf_node, slam_node])
